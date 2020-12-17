718c1b40d2b37f35ca5169deccbd35bd9e547f19
pedroloza <53161479+pedroloza@users.noreply.github.com>
2020-12-17 08:51:23 -0500

    Update Prueba1.sql

--- Prueba1.sql
+++ Prueba1.sql
@@ -1,1 +1,12 @@
-CREATE TABLE public.instructor (    "C.I " integer NOT NULL,    nombre character varying,    apellido character varying,    telefono integer,    prestacion_de_servicio character varying,    tiempo_cargo character varying,    numero_empresas_trabajadas character varyingª
+CREATE TABLE public."guardiaSeguridad"
+(
+    "ID" integer NOT NULL,
+    "C.I" integer,
+    "Nombre" character varying COLLATE pg_catalog."default",
+    apellido character varying COLLATE pg_catalog."default",
+    genero character varying COLLATE pg_catalog."default",
+    nacionalidad character varying COLLATE pg_catalog."default",
+    telefono integer,
+    fecha_de_nacimiento date,
+    CONSTRAINT "guardiaSeguridad_pkey" PRIMARY KEY ("ID")
+)
