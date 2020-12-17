aa6808750c1e0cfb6e39b86a6ffbaa9ff5def6b0
pedroloza <53161479+pedroloza@users.noreply.github.com>
2020-12-17 09:01:14 -0500

    Add files via upload

---
+++ Prueba1.sql
@@ -0,0 +1,1 @@
+CREATE TABLE public.instructor (    "C.I " integer NOT NULL,    nombre character varying,    apellido character varying,    telefono integer,    prestacion_de_servicio character varying,    tiempo_cargo character varying,    numero_empresas_trabajadas character varyingª
